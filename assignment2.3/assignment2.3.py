f323a11662fe4294291b149db59ee4210ee7aeec
Diallo Gentry <dgentry@Valere.local>
2019-01-08 22:01:51 -0600

    simple input and calculation script

---
+++ assignment2.3/assignment2.3.py
@@ -0,0 +1,16 @@
+# This assignment will obtain two pieces of data from the user and perform a calculation.
+
+# This script begins with a hint from the course...but lets do a little over-achievement.
+
+print("Alright, let's do our first calculation in Python using information obtained from the user.")
+
+# This first line is provided for you
+
+hrs = input("Please enter the number of hours worked: ")
+rate = input("Thank you. Now enter the rate ($/hr): ")
+
+# calculation...
+pay = hrs * rate
+
+# output
+print("Pay: " + pay)
