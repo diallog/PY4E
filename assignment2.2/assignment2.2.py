892e48dce922f991e6922e5c029ab32bdf354ed5
Diallo Gentry <dgentry@Valere.local>
2019-01-08 19:02:30 -0600

    assignment 2.2 folder and script

---
+++ assignment2.2/assignment2.2.py
@@ -0,0 +1,8 @@
+# First project asking from input from the user
+
+# The following lines are taken from the online Python emulator:
+
+# The code below almost works
+
+name = input("Enter your name:")
+print("Hello " + name)
