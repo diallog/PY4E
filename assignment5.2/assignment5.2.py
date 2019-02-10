a7afc5a99733cd3d1a4cb5813ac7e00672ebd34d
Diallo Gentry <dgentry@Valere.local>
2019-02-10 15:14:40 -0600

    minor cosmetic update

--- assignment5.2/assignment5.2.py
+++ assignment5.2/assignment5.2.py
@@ -1,6 +1,7 @@
 # Assignment 5.2 Write a program that repeatedly prompts a user for integer numbers until the user enters 'done'. Once 'done' is entered, print out the largest and smallest of the numbers. If the user enters anything other than a valid number catch it with a try/except and put out an appropriate message and ignore the number. Enter 7, 2, bob, 10, and 4 and match the output below.
 
 # Initialize variables
+
 # smallest = None # variable holds current smallest value
 # largest = None # variable holds current largest value
 newNumber = 0 # variable holds new input for testing
