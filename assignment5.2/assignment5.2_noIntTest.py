8ec5b4a82afd2a756365e6d6edca319487fac421
Diallo Gentry <dgentry@Valere.local>
2019-02-10 16:41:38 -0600

    removed extraneous

--- assignment5.2/assignment5.2_noIntTest.py
+++ assignment5.2/assignment5.2_noIntTest.py
@@ -5,25 +5,8 @@
 smallest = None # variable holds current smallest value
 largest = None # variable holds current largest value
 newNumber = 0 # variable holds new input for testing
-floatNewNumber = 0.0 # new input converted to float
 intNewNumber = 0 # new input converted to integer
-isIntegerResult = 0 # receives return value from isInteger()
-numbersToTest = list() # list created by user input
 request = True
-
-# Define function(s)
-
-def isInteger(possibleInt):
-	# tests to see if user-provided input is an integer
-	maybeEven = 2 * possibleInt
-	if (maybeEven % 2) > 0:
-		# maybeEven is not an integer; user needs to provide new value
-		# print(maybeEven,'- You did not provide a whole-number. Please try again.')
-		return 0
-	else:
-		# maybeEven is an integer and program execution can proceed
-		# print(maybeEven, '- Congratulations! You provided a whole number.') # temporary statement
-		return 1
 
 # Main program - build the list from user input
 
