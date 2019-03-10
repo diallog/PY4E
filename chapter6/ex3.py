d0e10ee840f3471d551cb7505099ac48d9ae1675
Diallo Gentry <dgentry@practicum.gee-zone.us>
2019-03-10 01:31:49 -0600

    initial commit

--- chapter6/ex3.py
+++ chapter6/ex3.py
@@ -8,11 +8,16 @@ for letter in word:
 print(count)
 """
 
-# Give instructions to the users
-print('''
-===========================================================================
-| Please provide a string and a target letter to search for.              |
-===========================================================================
-''')
+# Define function
+def myCount(usrInput1,usrInput2):
+	count = 0
+	for letter in usrInput1:
+		if letter == usrInput2:
+			count = count + 1
+	print(count)
 
-def count(word,letter)
+# Get user input
+mainInput1 = input('Give me a string (word, sentence..somthing): ')
+mainInput2 = input('Give me a letter to search for: ')
+
+myCount(mainInput1,mainInput2)
