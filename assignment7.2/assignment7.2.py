dbdf51d59bdce4d391d720064f4c1742ca2d9118
Diallo Gentry <dgentry@Valere.local>
2019-04-13 23:00:07 -0500

    showing confidence accuumulation

--- assignment7.2/assignment7.2.py
+++ assignment7.2/assignment7.2.py
@@ -27,7 +27,7 @@ for eaLine in fHandle:
 		print('Conversion failed. Please check data and try again')
 		exit()
 	totConfidence = totConfidence + flConfInterval
-	print(confInterval)
+	print(totConfidence)
 
 # print the total number of identified noLines
 # print('The total number of identified lines is: ',noLines)
