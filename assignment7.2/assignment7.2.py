9d81bbedbc0f8949d25cb154c792fbec4654eccd
Diallo Gentry <dgentry@Valere.local>
2019-04-13 23:01:26 -0500

    should be final step

--- assignment7.2/assignment7.2.py
+++ assignment7.2/assignment7.2.py
@@ -31,4 +31,4 @@ for eaLine in fHandle:
 
 # print the total number of identified noLines
 # print('The total number of identified lines is: ',noLines)
-print('Average spam confidence: ',numOfLines)
+print('Average spam confidence: ',totConfidence / numOfLines)
