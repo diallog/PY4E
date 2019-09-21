7edb4f96e4e3dd5bb11cfd475c9cea64cf680fd8
Diallo Gentry <dgentry@practicum.dev.gee-zone.us>
2019-09-20 23:10:47 -0500

    updates

--- assignment8.4/fileTest.py
+++ assignment8.4/fileTest.py
@@ -14,7 +14,16 @@ except:
 	print('Something bad happened. The requested file probably does not exist.')
 
 # Copy the contents of the orginal file
+
 # Write the original contents to the output files
+# Show that the two files are the same
+print("This is the orginal file:")
+for i in orginalFile: print(i.rstrip())
+print(\n\n)
+print("And here is the copy of the orginal:")
+for i in outputFile: print(i.rstrip())
+print(\n\n)
+
 # Close all the files
 orginalFile.close()
 outputFile.close()
