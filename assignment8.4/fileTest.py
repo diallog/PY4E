7bfff001877c3556376a618587be48356316b7cb
Diallo Gentry <dgentry@practicum.dev.gee-zone.us>
2019-09-20 22:32:39 -0500

    initial commit

---
+++ assignment8.4/fileTest.py
@@ -0,0 +1,23 @@
+```
+The purpose of this script will be to open a file, copy its content
+and write that output to another file
+```
+
+# Open the original file for reading
+try:
+	orginalFile = open('./romeo.txt', 'r')
+except:
+	print('Something bad happened. The requested file probably does not exist.')
+
+# Open the output file for writing
+try:
+	outputFile = open('./copyOfOriginal.txt', 'w')
+except:
+	print('Something bad happened. The requested file probably does not exist.')
+	break
+
+# Copy the contents of the orginal file
+# Write the original contents to the output files
+# Close all the files
+orginalFile.close()
+outputFile.close()
