e8932fc34fb83866b3d1e32841419bcd2d60c1e6
Diallo Gentry <dgentry@practicum.dev.gee-zone.us>
2019-09-20 23:36:58 -0500

    changing order of operations

--- assignment8.4/fileTest.py
+++ assignment8.4/fileTest.py
@@ -13,16 +13,21 @@ try:
 except:
 	print('Something bad happened. The requested file probably does not exist.')
 
-# Copy the contents of the orginal file
+# Print the empty output files
+print('And here is the empty output file:')
+for i in outputFile: print(i.rstrip())
+print('\n\n')
+
+# Show the original file
+print('This is the orginal file:')
+for i in orginalFile: print(i.rstrip())
+print('\n\n')
 
 # Write the original contents to the output file
 for i in orginalFile:
 	outputFile.write(i)
 
-# Show that the two files are the same
-print('This is the orginal file:')
-for i in orginalFile: print(i.rstrip())
-print('\n\n')
+# Show the output file after copying
 print('And here is the copy of the orginal:')
 for i in outputFile: print(i.rstrip())
 print('\n\n')
