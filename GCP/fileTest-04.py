7f4f3a6e1bfb139145c97c0e9b12d4dd5d6d8a35
Diallo Gentry <dgentry@Valere.local>
2020-01-12 12:20:34 -0600

    writing to a file

---
+++ GCP/fileTest-04.py
@@ -0,0 +1,14 @@
+'''This is an exercise to write multiple lines to a file'''
+
+# set some variables
+
+volume = [23, 96]
+
+# write strings to output
+
+outputFile = open('./theVolumesAre.txt', 'a')
+
+for i in range(0,1):
+	outputFile.write('Volume' + str(i + 1) + ' is: ' + volume[i])
+
+outputFile.close()
