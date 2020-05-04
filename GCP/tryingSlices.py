423861ca62a3ae03eb23ef1f131b57a35540a5a5
Diallo Gentry <dgentry@Valere.local>
2020-05-04 12:31:17 -0500

    new script

---
+++ GCP/tryingSlices.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+daysInMonth = [31,28,31,30,31,30,31,31,30,31,30,31]
+total = 0
+
+# For days in month, only output the days in summer and total them
+
+for i in daysInMonth[5:7]:
+    print (i)
+    total = total + i
+    print ("The total number of days in summer are {days}".format (days = total))
