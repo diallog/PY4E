548dec968e6e635450527e1b78a233792b554a07
Diallo Gentry <dgentry@Valere.local>
2020-05-04 12:32:42 -0500

    corrected loop

--- GCP/tryingSlices.py
+++ GCP/tryingSlices.py
@@ -5,7 +5,7 @@ total = 0
 
 # For days in month, only output the days in summer and total them
 
-for i in daysInMonth[5:7]:
+for i in daysInMonth[5:8]:
     print (i)
     total = total + i
-    print ("The total number of days in summer are {days}".format (days = total))
+print ("The total number of days in summer are {days}".format (days = total))
