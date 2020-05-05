c8e43d28461927fb97abdbe737fc8ae0042adcc6
Diallo Gentry <dgentry@Valere.local>
2020-05-05 11:19:17 -0500

    exercise complete

--- GCP/01_listExercise.py
+++ GCP/01_listExercise.py
@@ -1,28 +1,36 @@
 #!/usr/bin/env python3
 
-# input name and age for multiple people until entering 'stop'
+# obtain name and age data for multiple people until entering 'stop'
 # then report who is the oldest person
 # use lists for this exercise
 
 # initialize variables
-listIndex = 0
 nameList = []
 ageList = []
 newName = None
 newAge = 0
+maxAge = 0
+maxIndex = 0
 
+# get input
 newName = input ("What is the person's name? (enter 'stop' to quit collecting names.) ")
 newName = newName.lower()
 while newName != 'stop':
 	nameList.append(newName)
-	
+
 	newAge = input ("How old is {newName}? ".format(newName = newName.capitalize()))
 	newAge = int(newAge)
 	ageList.append(newAge)
-	
+
 	print ("Thank you. Name and age recorded for {newName}.".format(newName = newName.capitalize()))
+	print ("\n")
 	newName = input ("What is the person's name? (enter 'stop' to quit collecting names.) ")
 	newName = newName.lower()
 
-print (nameList)
-print (ageList)
+# process data
+maxAge = max(ageList)
+maxIndex = ageList.index(maxAge)
+
+# report results
+print ("\n")
+print ("{oldest} is the oldest person with an age of {age}.".format(oldest = nameList[maxIndex].capitalize(), age = ageList[maxIndex]))
