9a3429a432f0237d0f0c9da48f3a2d4487995e94
Diallo Gentry <dgentry@Valere.local>
2020-05-05 21:14:30 -0500

    continuing iteration

--- GCP/topDown01.py
+++ GCP/topDown01.py
@@ -2,6 +2,7 @@
 
 # initialize variables
 dataFile = None
+weatherList = []
 
 ############ read data ############
 # open file
@@ -9,13 +10,24 @@ dataFile = None
 dataFile = input("Please provide the name of the data file: ")
 with open(dataFile, 'r') as weatherData:
     for line in weatherData:
-        print(line)
-weatherData.close()
-# read file contents
-# close file
-
+        date, hiTemp, loTemp, rainFall = line.split(',')
+        month, day, year = date.split('/')
+        month = int(month)
+        day = int(day)
+        year = int(year)
+        hiTemp = int(hiTemp)
+        loTemp = int(loTemp)
+        rainFall = float(rainFall)
+        weatherList.append([year, month, day, loTemp, hiTemp, rainFall])
 
 ############ analyze data ############
+# get user input
+
+userDay = int(input("What day do you want to search for? (enter as one or two "
+              "digit number, e.g. 5 or 12): "))
+userMonth = int(input("What month do you want to search for? (enter as one or two "
+                "digit number, e.g. 5 or 12): "))
+
 
 
 ############ report results ############
