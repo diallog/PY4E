84aa8e944fa95134fc27d812f3b5fbcee5e7bb69
Diallo Gentry <dgentry@Valere.local>
2020-05-20 12:13:10 -0500

    recent file updates

--- GCP/fileTest-05.py
+++ GCP/fileTest-05.py
@@ -2,9 +2,23 @@
 
 # Some basic file manipulation trying alternate 'open' method.
 
+import os
+os.system('clear')
+
 with open ('00_test.txt', 'r') as inputForPlay:
 	#read the file and output to a list
 	theFile = inputForPlay.read()
-	# print(list(theFile))
-	splitFile = theFile.split()
-	print(sorted(splitFile))
+
+fileList = theFile.split()
+print("The original list is: \n", fileList, "\n")
+
+# sort() method sorts the list in place
+fileList.sort()
+print("The sorted list is: \n", fileList, "\n")
+
+'''
+**Notes for ongoing work**
+In order to get the sorted list sorted (the way I want it sorted), going to have
+to process fileList to be all lower case. Would be interesting to "remember" all
+of the items that were originally capitalized in the list.
+'''
---
+++ GCP/tupleTest.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+# define tuple
+testTuple = ("string 1", "string 2", "string 3")
+
+# save tuple valies to three separate variables
+x, y, z = testTuple
+
+# print variables
+print(x)
+print(y)
+print(z)
+
+# print tuple
+print(testTuple)
