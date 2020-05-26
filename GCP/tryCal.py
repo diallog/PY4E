cf263840e077e7309fffe4eae9ee203a4ae50fc4
diallog <github@gee-zone.us>
2020-05-26 11:50:18 -0500

    early use of modules

---
+++ GCP/tryCal.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+import calendar
+
+calendar.setfirstweekday((calendar.SUNDAY))
+print(calendar.month(2020,1))
---
+++ GCP/tryWeb.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+
+import webbrowser
+webbrowser.open("https://www.ionic.com")
