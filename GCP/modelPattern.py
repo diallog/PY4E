618dfa95cea8d3377cee68240fc54e58f76d982e
Diallo Gentry <dgentry@Valere.local>
2020-07-22 17:51:12 -0500

    added an empty py file showing useful structure pattern

---
+++ GCP/modelPattern.py
@@ -0,0 +1,13 @@
+#!#!/usr/bin/env python3
+
+# PURPOSE: observerd from https://youtu.be/rnatu3xxVQE; good pattern for py files
+
+def a_function():
+    # do some stuff
+
+def Main():
+    # do the main stuff
+
+if __name__ == '__main__'
+    # do all the stuff; at a minimum, this will invoke Main()
+    # if Main() is not defined as fucntion, that block can just go here
